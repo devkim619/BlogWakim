1fdf23f508b397d5cf1e9a7fb833f8180de4485f
WAKIM\khimk <khimk635@gmail.com>
2024-12-24 05:15:43 +0700

    no message

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+        provider: 'supabase',
+        providerOptions: {
+            supabaseUrl: env('SUPABASE_URL'),
+            supabaseApiKey: env('SUPABASE_API_KEY'),
+            bucket: env('SUPABASE_BUCKET'),
+            directory: env('SUPABASE_DIRECTORY'),
+        },
+    },
+});
