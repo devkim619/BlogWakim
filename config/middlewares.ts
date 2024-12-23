c9cb532bd6402a953f2376259e21d1dac10936b1
WAKIM\khimk <khimk635@gmail.com>
2024-12-24 05:53:09 +0700

    no message

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,37 +1,37 @@
-module.exports = ({ env }) => [
-  "strapi::errors",
+module.exports = ({ env }: { env: any }) => [
+  'strapi::errors',
   {
-    name: "strapi::security",
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "default-src": ["'self'"],
-          "img-src": [
+          'connect-src': ["'self'", 'https:'],
+          'default-src': ["'self'"],
+          'img-src': [
             "'self'",
-            "data:",
-            "blob:",
-            "market-assets.strapi.io",
-            env("SUPABASE_URL"),
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'https://cthkswwbreetaeneeuyi.supabase.co',  // ระบุ URL Supabase ตรงๆ
           ],
-          "media-src": [
+          'media-src': [
             "'self'",
-            "data:",
-            "blob:",
-            "market-assets.strapi.io",
-            env("SUPABASE_URL"),
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'https://cthkswwbreetaeneeuyi.supabase.co',  // ระบุ URL Supabase ตรงๆ
           ],
         },
       },
     },
   },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,17 +1,16 @@
-module.exports = ({ env }) => ({
+module.exports = ({ env }: { env: any }) => ({
     upload: {
         config: {
-            provider: "strapi-provider-upload-supabase",
+            provider: 'strapi-provider-upload-supabase',
             providerOptions: {
-                apiUrl: env("SUPABASE_API_URL"),
-                apiKey: env("SUPABASE_API_KEY"),
-                bucket: env("SUPABASE_BUCKET"),
-                directory: env("SUPABASE_DIRECTORY"),
+                apiUrl: env('SUPABASE_API_URL'),
+                apiKey: env('SUPABASE_API_KEY'),
+                bucket: env('SUPABASE_BUCKET'),
+                directory: env('SUPABASE_DIRECTORY'),
                 options: {},
             },
             actionOptions: {
-                upload: {},
-                uploadStream: {},
+                upload: {}, // ใช้แค่ upload
                 delete: {},
             },
         },
