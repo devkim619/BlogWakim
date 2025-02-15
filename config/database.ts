fa67315c1d42aeba74bc8f59d6f43dde0162f806
Surapat Wongsri <khimnk635@gmail.com>
2025-02-15 20:10:20 +0700

    no message

--- config/database.ts
+++ config/database.ts
@@ -24,16 +24,19 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
-        connection: {
-          connectionString: env('DATABASE_URL'),
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapi'),
-          user: env('DATABASE_USERNAME', 'strapi'),
-          password: env('DATABASE_PASSWORD', 'strapi'),
-          ssl: false, // ตั้งค่า ssl เป็น false
-          schema: env('DATABASE_SCHEMA', 'public'),
-        },
+        connectionString: env('DATABASE_URL'),
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false) ? {
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+          ca: env('DATABASE_SSL_CA', undefined),
+          key: env('DATABASE_SSL_KEY', undefined),
+          cert: env('DATABASE_SSL_CERT', undefined),
+        } : false,
+        schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
